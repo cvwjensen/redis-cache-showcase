0d9f96d41577f51c0604ecb204e11f03813b2d7d
cvwj <cvwjensen@gmail.com>
2023-12-12 07:35:28 +0100

    Showcase 2nd level caching with hibernate and redis

--- src/main/java/dk/lundogbendsen/springbootcourse/rediscache/RedisCacheApplication.java
+++ src/main/java/dk/lundogbendsen/springbootcourse/rediscache/RedisCacheApplication.java
@@ -2,14 +2,16 @@ package dk.lundogbendsen.springbootcourse.rediscache;
 
 import dk.lundogbendsen.springbootcourse.rediscache.jpa.PersonRepository;
 import dk.lundogbendsen.springbootcourse.rediscache.jpa.model.Person;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 
+import java.util.Optional;
+
 @SpringBootApplication
-@EnableCaching
 public class RedisCacheApplication {
 
     public static void main(String[] args) {
@@ -19,23 +21,17 @@ public class RedisCacheApplication {
     @Bean
     CommandLineRunner runner(PersonRepository personRepository) {
         return args -> {
-            personRepository.save(new Person("Lars", 21L));
+            Logger logger = LoggerFactory.getLogger(RedisCacheApplication.class);
+            logger.info("LOOKING FOR PERSON WITH ID 1");
+            Optional<Person> person = personRepository.findById(1L);
+            logger.info("LOOKING FOR PERSON WITH ID 666");
+            person = personRepository.findById(666L);
+            logger.info("SAVING PERSON WITH ID 1");
+            Person lars = personRepository.save(new Person("Lars", 21L));
+            logger.info("LOOKING FOR PERSON WITH ID 1 AGAIN");
+            person = personRepository.findById(1L);
+            logger.info("lars.getName() = " + person.get().getName());
         };
     }
-//    @Bean
-    CommandLineRunner runner(ServiceCached serviceCached) {
-        return args -> {
 
-            System.out.println("serviceCached = " + serviceCached.getClass());
-            System.out.println("Is 2 prime? " + serviceCached.isPrime(2));
-            System.out.println("Is 2 prime? " + serviceCached.isPrime(2));
-
-            System.out.println("Is 3 prime? " + serviceCached.isPrime(3));
-            System.out.println("Is 3 prime? " + serviceCached.isPrime(3));
-
-            System.out.println("Is 4 prime? " + serviceCached.isPrime(4));
-            System.out.println("Is 4 prime? " + serviceCached.isPrime(4));
-            System.out.println("Is 2 prime? " + serviceCached.isPrime(920000009876035001L));
-        };
-    }
 }
