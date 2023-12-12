c3695c4818daf941ac29b307542b50f2a7d5da1d
cvwj <cvwjensen@gmail.com>
2023-12-12 08:06:49 +0100

    Exercises for Redis caching

--- src/main/java/dk/lundogbendsen/springbootcourse/rediscache/RedisCacheApplication.java
+++ src/main/java/dk/lundogbendsen/springbootcourse/rediscache/RedisCacheApplication.java
@@ -1,8 +1,10 @@
 package dk.lundogbendsen.springbootcourse.rediscache;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RedisCacheApplication {
@@ -10,6 +12,5 @@ public class RedisCacheApplication {
     public static void main(String[] args) {
         SpringApplication.run(RedisCacheApplication.class, args);
     }
-
 
 }
